125ca63aa2955984b48ebd62825b7ab4e15244a9
Karla-Flores <ms.kaflores@gmail.com>
2021-06-18 17:19:17 -0500

    List the department of each employee with the following information: employee number, last name, first name, and department name.

--- EmployeeSQL/Data_Analysis.sql
+++ EmployeeSQL/Data_Analysis.sql
@@ -1,14 +1,21 @@
--- Employee details: employee number, last name, first name, sex, and salary.
+-- List the following details of each employee: employee number, last name, first name, sex, and salary.
 
 SELECT "Employees"."empl_no", "Employees"."last_name", "Employees"."first_name","Employees"."sex","Salaries"."salary"
 FROM "Employees", "Salaries"
 WHERE "Employees"."empl_no" = "Salaries"."empl_no";
 
--- Employees who were hired in 1986. Details: first name, last name, and hire date.
+-- List first name, last name, and hire date for employees who were hired in 1986.
 
 SELECT "Employees"."first_name", "Employees"."last_name", "Employees"."hire_date"
 FROM "Employees"
 WHERE "Employees"."hire_date" BETWEEN '1986-01-01' AND '1986-12-31';
 
--- Manager of each department details: department number, department name, the manager's employee number, last name, first name.
-SELECT * FROM "Employees";
+-- List the manager of each department with the following information: department number, department name, the manager's employee number, last name, first name
+
+SELECT "Departments"."dept_no", "Departments"."dept_name", "Manager"."empl_no", "Employees"."last_name", "Employees"."first_name"
+FROM "Departments" 
+JOIN "Manager" ON "Departments"."dept_no" = "Manager"."dept_no"
+JOIN "Employees" ON "Manager"."empl_no" = "Employees"."empl_no"; 
+
+-- List the department of each employee with the following information: employee number, last name, first name, and department name.
+
